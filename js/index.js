873f29ffbe9fbf3e1859c8f2704ee5500b1b7269
abdoaswe <abdoahmed4434@gmail.com>
2022-11-30 20:41:45 +0200

    first step

---
+++ js/index.js
@@ -0,0 +1,88 @@
+////////////////////////////task 1 /////////////////////////////////////
+
+// var x=prompt("enter the temperature");
+
+// if(x>30){
+//     console.log("Hot");
+// }
+// else if (x<30)
+
+// console.log("cold");
+
+
+/////////////////////////////////task 2/////////////////////////////////////
+// var x=0;
+// var num = prompt("entar your name");
+// while((isFinite(num))&& x<=3){
+//     x++;
+//     alert(x+"is Number try agian");
+// var num = prompt("entar your name");
+
+// }
+// console.log("is a string");
+ 
+
+// var i=0;
+
+// var birth= prompt("entar your Brith day");
+
+// while(isNaN(birth)||birth<=2010 && i<3){
+//     i++;
+//     alert(i+"  try agian");
+// var birth = prompt("entar your Brith day");
+
+// }
+// var age = 2022-birth;
+// console.log (age );
+
+
+////////////////////////////////// task 3 /////////////////////////////////////////
+
+//  let a =prompt("entar first number");
+//  let b =prompt("entar scand number");
+//  let c =prompt("entar third number");
+
+// let y =(b*b)-(4*a*b);
+// console.log(y);
+
+// let x=(b + (Math.sqrt(y)/(2*a)));
+// let x2=(b-(Math.sqrt(y)/(2*a)));
+   
+
+//  var result =a*(Math.pow(x,2))+b*(x)+c;
+// console.log(parseInt(result) );
+
+// var result1 =a*(Math.pow(x2,2))+b*(x2)+c;
+// console.log(parseInt(result1) );
+
+
+/////////////////////////////////task 4 //////////////////////////////////////
+
+
+// let n =prompt("entar first number");
+// let m =prompt("entar sacond Number");
+
+// n=parseInt(n.slice(-1)) ;
+// m=parseInt(m.slice(-1)) ;
+
+// let result =n+m;
+
+
+// console.log(result);
+
+
+
+
+/////////////////////////task 5 ///////////////////////////////
+
+const x = 60; //K/m 
+const y = 90; //K/m 
+
+var distance =prompt("enter the distance")
+
+let car1 = x/60*distance;
+let car2 = y/60*distance;
+ console.log(car1+"K/m" ,car2+"k/m");
+
+
+
