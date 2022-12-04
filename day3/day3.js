dfa7a7e364b392cfd6540c08fa824ac04a630589
abdoaswe <abdoahmed4434@gmail.com>
2022-12-04 19:34:31 +0200

    day3

---
+++ day3/day3.js
@@ -0,0 +1,116 @@
+///////////////task 1//////////////////////
+// var x;
+// function show(){
+
+//     x=window.open(""+""+"_blank")
+//     x.document.write("hellow world")
+// }
+
+// function closewin(){
+
+//      x.close()
+    
+// }
+
+
+////////////////////////task2////////////////////////
+
+
+ 
+// function showclock(){
+//     setInterval(showclock,1000)
+//     var date= new Date();
+   
+//     document.getElementById("show").innerHTML=date;
+
+// }
+
+
+////////////////////////////task 3///////////////////
+
+
+// var x;
+// var i=0;
+
+
+
+// function show(){
+
+//     setTimeout(()=>{
+    
+//     x=window.open(""+""+"_blank")
+     
+   
+
+//    var tex=document.getElementById("p").innerText;
+
+//   setInterval(()=> {
+//     x.document.write(tex[i++])
+//                    }, 1000);
+ 
+    
+// },3000)
+    
+//  }
+
+
+
+
+
+////////////////////////////task 4////////////////////////////
+
+// function scorling()
+// {
+//     if(scrollY>777)
+//     {
+//         // scrollTo(0,0)
+//         alert("hiiii")
+//     }
+// }
+
+
+/////////////////////////////task 5 ///////////////////////
+
+
+// var x;
+// function show(){
+      
+//     x=window.open(""+""+"_blank")
+     
+//    var tex=document.getElementById("text").value;
+  
+//     x.document.write(tex)
+//     setTimeout(()=>{
+//          x.close()
+    
+//            },3000)
+    
+// }
+
+
+/////////////////////////////task 6/////////////////////////////
+
+
+// var x;
+
+// function show(){
+      
+//   var x=window.open("", "", "_blank");
+    
+ 
+// //   .style.backgroundColor = "lightblue";
+// x.document.body.setAttribute("style","height:7000px;");
+  
+// x.document.body.setAttribute("onscroll","scrollY");
+// var child= x.document.createElement("p");
+// child.innerHTML="Hallow";
+// child.style.color="red"
+// x.document.body.appendChild(child);
+
+
+
+
+    
+// }
+
+
